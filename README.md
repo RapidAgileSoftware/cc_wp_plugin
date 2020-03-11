0ece49b7f13e6ac6069ab62d0778c240e85a912d
Jens Krause <jens@rasta.online>
2020-03-11 13:17:11 +0700

    feat(structural) initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Cookiecutter project template
+
+To create a new project using this template, do:
+
+    pip install cookiecutter pip-tools
+    cookiecutter gh:RapidAgileSoftware/cc_wp_plugin
---
+++ {{cookiecutter.plugin_slug}}/includes/class-{{cookiecutter.plugin_slug}}-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    {{cookiecutter.plugin_name}}
+ * @subpackage {{cookiecutter.plugin_name}}/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    {{cookiecutter.plugin_name}}
+ * @subpackage {{cookiecutter.plugin_name}}/includes
+ * @author     Your Name <email@example.com>
+ */
+class {{cookiecutter.plugin_name}}_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
